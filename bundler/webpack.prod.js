0f87e1b99084e1e0d36f585388c27fdf5396b579
philszalay <philszalay@aol.de>
2022-06-23 01:21:42 +0700

    update project structure

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -2,13 +2,15 @@ const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = merge(
-    commonConfiguration,
+module.exports = (env, argv) => {
+  return merge(
+    commonConfiguration(env, argv),
     {
-        mode: 'production',
-        plugins:
+      mode: 'production',
+      plugins:
         [
-            new CleanWebpackPlugin()
+          new CleanWebpackPlugin()
         ]
     }
-)
+  )
+}
